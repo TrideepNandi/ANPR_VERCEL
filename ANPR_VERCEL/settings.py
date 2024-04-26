ff46aab5611dbbd997788f9acd651475cde4b25c
Trideep Nandi <nandi.trideep2003@gmail.com>
2024-04-26 19:59:25 +0530

    vercel deployment

--- ANPR_VERCEL/settings.py
+++ ANPR_VERCEL/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*', '127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -83,7 +83,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ANPR_VERCEL.wsgi.application"
+WSGI_APPLICATION = "ANPR_VERCEL.wsgi.app"
 
 
 # Database
