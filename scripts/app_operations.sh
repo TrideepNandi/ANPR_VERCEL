34f182d5e827d164adb74781f083f9ba6f0c12ae
Trideep Nandi <nandi.trideep2003@gmail.com>
2024-04-27 01:52:52 +0530

    fixed the code for deployment

--- scripts/app_operations.sh
+++ scripts/app_operations.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Django operations
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/ANPR_VERCEL/ANPR_VERCEL/settings.py
+# sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/ANPR_VERCEL/ANPR_VERCEL/settings.py
+sed -i 's/\[]/\["3.110.161.152"]/' /home/ubuntu/ANPR_VERCEL/ANPR_VERCEL/settings.py
 python3 manage.py migrate
 python3 manage.py makemigrations
 python3 manage.py collectstatic
---
+++ scripts/before_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git clone https://github.com/TrideepNandi/ANPR_VERCEL.git /home/ubuntu/
+
+# Create a virtual environment and activate it
+virtualenv /home/ubuntu/env
+source /home/ubuntu/env/bin/activate
+
+# Install Python dependencies
+pip3 install -r /home/ubuntu/ANPR_VERCEL/requirements.txt
+
+# Copy Gunicorn service and socket files
+sudo cp /home/ubuntu/ANPR_VERCEL/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/ANPR_VERCEL/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
+
+# Copy Nginx configuration
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo cp /home/ubuntu/ANPR_VERCEL/nginx/nginx.conf /etc/nginx/sites-available/ANPR_VERCEL
+sudo ln -s /etc/nginx/sites-available/ANPR_VERCEL /etc/nginx/sites-enabled/
