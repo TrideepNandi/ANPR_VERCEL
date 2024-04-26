2df950fb493a99ee63a1a096f676b67f10ce1fe7
Trideep Nandi <nandi.trideep2003@gmail.com>
2024-04-26 19:33:22 +0530

    added some files and changed some configuration for the vercel deployment followed medium https://medium.com/@emusyoka759/hosting-a-django-application-on-vercel-a-step-by-step-guide-9cf33e2a1ecf

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+#! /usr/bin/env
+echo "Building prject packages..."
+python3 -m pip install -r requirements.txt
+
+echo "Migrating Database... "
+python3 manage. py makemigrations --noinput
+python3 manage. py migrate --noinput
+
+echo "Collecting static files... "
+python3 manage. py collectstatic --noinput
